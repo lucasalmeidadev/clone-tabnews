390a089b635634b936b13bfa5c0d8623a1924efd
Lucas Alexandre de Almeida <lucas.alexandre.almeida@gmail.com>
2026-02-17 15:28:31 -0300

    feat: invalidate session cookie on unauthorized error

--- infra/controller.js
+++ infra/controller.js
@@ -14,11 +14,12 @@ function onNoMathHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
-  if (
-    error instanceof ValidationError ||
-    error instanceof NotFoundError ||
-    error instanceof UnauthorizedError
-  ) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
+    return response.status(error.statusCode).json(error);
+  }
+
+  if (error instanceof UnauthorizedError) {
+    clearCookieSession(response);
     return response.status(error.statusCode).json(error);
   }
 
